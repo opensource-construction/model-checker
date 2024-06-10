5f1574e65c763aed9cd8e8ba5eacb36616788e4d
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-06-10 18:55:38 +0200

    fixed #27 - mobile layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
 import oscLogo from 'assets/osc_logo.png'
-import { Group, Title, UnstyledButton } from '@mantine/core'
+import { Group, Title, UnstyledButton, useMatches } from '@mantine/core'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 export const Header = () => {
   const navigate = useNavigate()
   const { i18n } = useTranslation()
+  const imageHeight = useMatches({
+    base: '24px',
+    lg: '36px',
+  })
 
   return (
     <Group justify='space-between'>
       <UnstyledButton onClick={() => navigate('/')}>
         <Group pt={8}>
-          <img src={oscLogo} alt={'opensource.construction'} style={{ maxHeight: '36px' }} />
-          <Title order={3}>IFC Model Checker</Title>
+          <img src={oscLogo} alt={'opensource.construction'} style={{ maxHeight: imageHeight }} />
+          <Title order={4}>IFC Model Checker</Title>
         </Group>
       </UnstyledButton>
       <UnstyledButton onClick={() => i18n.changeLanguage(i18n.resolvedLanguage === 'en' ? 'en' : 'de')} mr='md'>
